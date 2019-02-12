495c5fd32e8764cc833174828f5a64959daf8cfe
Paul-Claudiu Orha <paul.orha@halcyonmobile.com>
2019-02-12 21:53:10 +0200

    This commit adds action messages for deposits and withdraws and handles unsuccessful withdraws

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ mod bank;
 fn main() {
     let mut bank = FirstBank::new();
     bank.deposit(100);
+    println!("Successful deposit");
     println!("Current balance: {}", bank.balance_inquiry());
-    bank.withdraw(30);
+    if bank.withdraw(130) {
+        println!("Successful withdraw")
+    } else {
+        println!("Invalid withdraw, not enough funds!")
+    }
     println!("Current balance: {}", bank.balance_inquiry());
 }
